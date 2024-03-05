13e2192b0d482735e072de67e2007908e2e3587b
afrin823 <afrin12490@gmail.com>
2024-03-05 22:42:05 +0600

    change css file name

--- todos/todos.js
+++ todos/todos.js
@@ -21,28 +21,29 @@ function displayAlbums(data){
         </div>
         <div class="card w-96 bg-[#ffeaa7] mb-8 shadow-xl mx-auto">
         <div class="card-body">
-          <h2 class="card-title text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">Todos</h2>
-          <h2 class="text-2xl text-center">${todos.userId}</h2>
+          <h2 class="card-title text-center">Todos</h2>
+          <h2 class="text-4xl text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">${todos.userId}</h2>
           <h2 class="text-2xl text-center">${todos.id}</h2>
           <h2 class="text-2xl text-center">${todos.title}</h2>
           </div>
         </div>
         <div class="card w-96 bg-[#ffeaa7] mb-8 shadow-xl mx-auto">
         <div class="card-body">
-          <h2 class="card-title text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">Todos</h2>
-          <h2 class="text-2xl text-center">${todos.userId}</h2>
+          <h2 class="card-title text-center">Todos</h2>
+          <h2 class="text-4xl text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">${todos.userId}</h2>
           <h2 class="text-2xl text-center">${todos.id}</h2>
           <h2 class="text-2xl text-center">${todos.title}</h2>
           </div>
         </div>
         <div class="card w-96 bg-[#ffeaa7] mb-8 shadow-xl mx-auto">
         <div class="card-body">
-          <h2 class="card-title text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">Todos</h2>
-          <h2 class="text-2xl text-center">${todos.userId}</h2>
+          <h2 class="card-title text-center">Todos</h2>
+          <h2 class="text-4xl text-center rounded-full bg-[#81ecec] w-24 h-24 todos-align">${todos.userId}</h2>
           <h2 class="text-2xl text-center">${todos.id}</h2>
           <h2 class="text-2xl text-center">${todos.title}</h2>
           </div>
         </div>
+        
 
      
         `;      
